c684ed15ffeb6d0fefabd6d5ea9faf9ffd99fc1c
MustafaMansoor <mustafamansoor47@gmail.com>
2022-07-20 02:09:46 +0500

    starting

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//const root = ReactDOM.createRoot(document.getElementById('root'));
-ReactDOM.render( <
-    div > hello < /div>,
-    document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render( <
+    App / >
 );
 
 // If you want to start measuring performance in your app, pass a function
